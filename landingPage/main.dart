8dd746c060df6ad0fa1b7d2167d8e073c7abce13
Anindya Lokeswara <anindya.lokeswara@ui.ac.id>
2022-11-27 14:22:01 +0700

    Menambahkan model dan fetch data web app nutrirobo

--- landingPage/main.dart
+++ landingPage/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'drawer.dart';
+import 'page/drawer.dart';
 
 final List<String> imgList = [
   'assets/nutriroboImage.jpg',
--- landingPage/page/drawer.dart
+++ landingPage/page/drawer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'main.dart';
+import '../main.dart';
 
 class MyDrawer extends StatefulWidget {
   const MyDrawer({super.key});
