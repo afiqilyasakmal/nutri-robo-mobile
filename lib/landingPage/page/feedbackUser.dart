93524a7e248bde1196eb5a381dd69743551f852c
anindyalkwr <anindya.lokeswara@ui.ac.id>
2022-12-07 02:17:14 +0700

    Menyelesaikan login dan signup (85% Done modul landing page)

---
+++ lib/landingPage/page/feedbackUser.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:nutrirobo/landingPage/page/fetchFeedbackUser.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../model/feedbackItem.dart';
+
+class FeedbackUserPage extends StatefulWidget {
+  const FeedbackUserPage({super.key});
+
+  @override
+  State<FeedbackUserPage> createState() => _FeedbackUserPageState();
+}
+
+class _FeedbackUserPageState extends State<FeedbackUserPage> {
+  @override
+  Widget build(BuildContext context) {
+    // The rest of your widgets are down below
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Row(
+            children: [
+              Image.asset(
+                'assets/logo.jpg',
+                height: 70,
+                width: 150,
+              ),
+            ],
+          ),
+        ),
+        body: FutureBuilder(
+            future: fetchFeedbackUser(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 30.0),
+                  child: Center(
+                      child: CircularProgressIndicator(
+                    color: Color.fromRGBO(38, 70, 85, 1),
+                  )),
+                );
+              } else {
+                if (snapshot.data.length == 0) {
+                  return Column(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 30.0),
+                        child: Center(
+                          child: Text(
+                            "Belum ada feedback ditambahkan",
+                            style: TextStyle(
+                                color: Color.fromRGBO(38, 70, 85, 1),
+                                fontSize: 20,
+                                fontFamily: 'Aubrey'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                } else {
+                  return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (_, index) => Container(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 8),
+                            padding: const EdgeInsets.all(20.0),
+                            decoration: BoxDecoration(
+                                image: const DecorationImage(
+                                    image: AssetImage('assets/cardImage.jpg'),
+                                    fit: BoxFit.cover),
+                                borderRadius: BorderRadius.circular(15.0),
+                                boxShadow: const [
+                                  BoxShadow(
+                                      color: Color.fromRGBO(38, 70, 85, 1),
+                                      blurRadius: 2.0)
+                                ]),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Rating: ${snapshot.data![index].fields.rating} / 10",
+                                  textAlign: TextAlign.right,
+                                  style: const TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromRGBO(38, 70, 85, 1)),
+                                ),
+                                Text(
+                                  snapshot.data![index].fields.date
+                                      .toString()
+                                      .substring(0, 10),
+                                  textAlign: TextAlign.right,
+                                  style: const TextStyle(fontSize: 14.0),
+                                ),
+                                const SizedBox(height: 10),
+                                const Text(
+                                  "Feedback:",
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromRGBO(38, 70, 85, 1)),
+                                ),
+                                Text(
+                                  "${snapshot.data![index].fields.feedback}",
+                                  style: const TextStyle(
+                                    fontSize: 14.0,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ));
+                }
+              }
+            }));
+  }
+}
